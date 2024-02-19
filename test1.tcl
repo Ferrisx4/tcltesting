5ce37dfe763217e29351f5400896d2d9d49432c0
Ferrisx4 <ferrisx4@gmail.com>
2024-02-19 15:32:27 -0500

    idk

---
+++ test1.tcl
@@ -0,0 +1,12 @@
+#!/usr/bin/tclsh
+
+# puts "Hello, world!"
+# puts [expr 3+2]
+
+# set thing 5.22
+# set thing2 830.3
+# set tcl_precision 4
+# puts [expr $thing2 / $thing]
+
+set thing3 1
+puts [expr $thing3 << 2]
